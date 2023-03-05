6c0fccffe6c40e61e63aabdd2ac11090ee8d7ac0
Mohit CHoudhary <mohit.choudhary@sap.com>
2023-03-05 07:10:23 +0530

    .

--- data/trustData.js
+++ data/trustData.js
@@ -7,7 +7,7 @@ const trustData = {
         "id": 1,
         "title": "श्री जाट चेरिटेबल ट्रस्ट, बाड़मेर",
         "description": "Barmer",
-        "imgSrc": '/assets/hostel/jat-charitable-barmer.jpg',  //missing
+        "imgSrc": '/assets/hostel/default.jpeg',  //missing
         "data": '/assets/hostel/default.pdf', //missing
         // "href": "jat-charitable-barmer",
         "href": "default",
@@ -16,7 +16,7 @@ const trustData = {
         "id": 2,
         "title": "युवा चोपाल ट्रस्ट, बाडमेर",
         "description": "Barmer",
-        "imgSrc": '/assets/hostel/yuva-choupal-barmer.jpg',  //missing
+        "imgSrc": '/assets/hostel/default.jpeg',  //missing
         "data": '/assets/hostel/yuva-choupal-barmer.pdf', //missing
         "href": "yuva-choupal-barmer",
       },
@@ -24,7 +24,7 @@ const trustData = {
         "id": 3,
         "title": "सेठ रामलाल सऊ चौथी देवी ट्रस्ट, बाडमेर",
         "description": "Barmer",
-        "imgSrc": '/assets/hostel/seth-ramlal-barmer.jpg',  //missing
+        "imgSrc": '/assets/hostel/default.jpeg',  //missing
         "data": '/assets/hostel/default.pdf', //missing
         // "href": "seth-ramlal-barmer",
         "href": "default",
@@ -35,18 +35,18 @@ const trustData = {
           "id": 1,
           "title": "ग्रामोत्थान विद्या पीठ संस्थान, डीडवाना",
           "description": "Nagore",
-          "imgSrc": '/assets/hostel/kuchagramothan-didwana-nagore.jpg',  //missing
-          "data": '/assets/hostel/kuchagramothan-didwana-nagore.pdf', //missing
-          "href": "kuchagramothan-didwana-nagore",
+          "imgSrc": '/assets/hostel/default.jpeg',  //missing
+          "data": '/assets/hostel/default.pdf', //missing
+          "href": "default",
           "status":  "working"
         },
         {
           "id": 2,
           "title": "वीर तेजा महिला शिक्षण शोध संस्थान तेजस्थली मारवाड़ मूडना",
           "description": "Nagore",
-          "imgSrc": '/assets/hostel/mudna-nagore.jpg',  //missing
-          "data": '/assets/hostel/mudna-nagore.pdf', //missing
-          "href": "mudna-nagore",
+          "imgSrc": '/assets/hostel/default.jpeg',  //missing
+          "data": '/assets/hostel/default.pdf', //missing
+          "href": "default",
           "status":  "working"
         }
       ],
