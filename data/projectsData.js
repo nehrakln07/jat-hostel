ec053ea9e6cc2f53431ae0abf4d1e7f969480844
Mohit CHoudhary <mohit.choudhary@sap.com>
2023-03-05 07:21:26 +0530

    .

--- data/projectsData.js
+++ data/projectsData.js
@@ -122,7 +122,7 @@ const projectsData = {
       "title": "श्री किसान बालिका छात्रावास, चौहटन",
       "description": "Barmer",
       "imgSrc": '/assets/hostel/kisan-girls-chohtan-barmer.jpg',
-      "data": '/assets/hostel/kisan-balika-barmer.pdf',  //missing
+      "data": '/assets/hostel/kisan-girls-chohtan-barmer.pdf',  //missing
       "href": "kisan-girls-chohtan-barmer",
       "status":  "working"
     },
