d4b1f7523049c4de7a45f6cf7dfcc99d8a8d2914
nehrakln07 <kamleshnehra252@gmail.com>
2023-03-15 13:22:45 +0530

    pushed changes

--- pages/career-counselors.js
+++ pages/career-counselors.js
@@ -1,8 +1,88 @@
 import SeoHead from "../components/SeoHead";
 import Layout from "../components/Layout/Layout";
 import PdfViewer from "../components/PdfViewer";
-export default function Courses() {
 
+const COUNSELERS = [
+    {
+        name: 'Dooda Ram Huda RAS ',
+        designation: 'SDM Reodar Sirohi',
+        qualification: 'MA(English, History,Pol.Science) Bed',
+        contact: '9314963657, 7976310852, Saturday -Sunday',
+        experties: 'RAS & Other Competition Exam ',
+        address: 'V/P -Rampura Dhorimna Barmer'
+    },{
+        name: 'BHera Ram Potliya IRS (RAS-2006 Batch)',
+        designation: 'Additional-Commission (IT) Udaipur',
+        qualification: 'BSTC, B.Ed. , MAC, English',
+        contact: '95304000175, Saturday -Sunday full time',
+        experties: 'IAS/RAS/NEET & Other Competition ',
+        address: 'V/P- Khara,Rathodan,Barmer'
+    },{
+        name: 'Narpat Jani',
+        designation: 'Teacher',
+        qualification: 'Coaching Barmer, BSC, MSC',
+        contact: '9509868337',
+        experties: 'All Compatition,RPSC,RSMSSB',
+        address: 'V/P-Redana,Barmer'
+    },{
+        name: 'Gumna Ram RPS',
+        designation: 'Additional S.P',
+        qualification: 'MA (Sanskrit & Pol. Science)',
+        contact: '9413810464, Saturday -Sunday',
+        experties: 'RAS/SI Raj. Police/CUET/RPSC Interview.',
+        address: 'V/P- Anta,Ramsar,Barmer'
+    },{
+        name: 'Deraj Ram RAS',
+        designation: 'A.O-Govt.Medical Collage, Barmer',
+        qualification: 'M.A-Pol.Science	',
+        contact: '9001557805, Saturday -Sunday Evening',
+        experties: 'RPSC Exam Specially RAS',
+        address: 'V/P – Kanod,Barmer'
+    },{
+        name: 'Govind Dudi ',
+        designation: 'C.A in KOTA',
+        qualification: 'CA, M.com, CCCA	',
+        contact: '9694002682 ANY DAYS 1 PM TO 4 PM',
+        experties: 'Commerce and CA,CS',
+        address: 'Add. 10-L-5 Mahavir Nager Kota'
+    },{
+        name: 'Chimna Ram Jani',
+        designation: 'Senior Teacher',
+        qualification: 'M.A,B.Ed',
+        contact: '9828581178,All days except school time',
+        experties: '	Counselling about NEET,CUET,All',
+        address: 'V/P- Bhimarlai Station Barmer'
+    },{
+        name: 'Dr. Bharat Saran',
+        designation: 'Doctor',
+        qualification: 'MBBS',
+        contact: '9413942612 (1 PM TO 3 PM)',
+        experties: 'NEET, CUET, JET, B.Sc-Nursing, Paramedical,IIT',
+        address: 'Barmer'
+    },{
+        name: 'Joga Ram',
+        designation: 'Former teacher - Navoday Vidyalya',
+        qualification: 'Lecturer, Biology',
+        contact: '70149-27574 , Sunday and Evening time',
+        experties: 'NEET,JEE,CUET,Navodya school Teachor',
+        address: 'V/P- G.SSS NimbalKot'
+    },{
+        name: 'Kmalesh Nehra',
+        designation: 'Software Engineer',
+        qualification: 'Btech MNNIT Allahabad',
+        contact: '8000822971',
+        experties: 'JEE, NEET, IT, Software Development, Career, Personal Development',
+        address: 'Bamnor Barmer'
+    },,{
+        name: 'Mohit Choudhary',
+        designation: 'Software Engineer',
+        qualification: 'Btech SRM Chennai',
+        contact: '9413988756',
+        experties: 'JEE, NEET, IT, Software Development, Career, Personal Development',
+        address: 'Gandhi Nagar Barmer'
+    }
+]
+export default function Courses() {
     return (
         <Layout>
             <SeoHead title='Career-counselors' />
@@ -12,8 +92,78 @@ export default function Courses() {
                     Career-counselors
                 </h1>
                 </div>
-                <div className="container py-4 ">
-                    <PdfViewer fileUrl={`/assets/default.pdf`} />
+                <div className="container py-4 w-full">
+                    {/* <PdfViewer fileUrl={`/assets/default.pdf`} /> */}
+                        <table className="w-full divide-y divide-gray-200 bg-white-500" style={{minWidth: 800}}>
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-lg font-bold text-left uppercase "
+                                    >
+                                        Name 
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-lg font-bold text-left uppercase "
+                                    >
+                                        Designation and Posting place  
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-lg font-bold text-left uppercase "
+                                    >
+                                        Qualification 
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-lg font-bold text-right uppercase "
+                                    >
+                                        Contact No. with suitable time and Days
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-lg font-bold text-right uppercase "
+                                    >
+                                        Interested Career Guidance Subject and Field 
+                                    </th>
+                                    <th
+                                        scope="col"
+                                        className="px-6 py-3 text-lg font-bold text-right uppercase "
+                                    >
+                                        Address
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-200">
+                                {
+                                    COUNSELERS.map((member, key) =>{
+                                        return(
+                                        <tr key={key}>
+                                            <td className="px-6 py-4 text-sm font-medium text-gray-800">
+                                                {member.name}
+                                            </td>
+                                            <td className="px-6 py-4 text-sm text-gray-800">
+                                                {member.designation}
+                                            </td>
+                                            <td className="px-6 py-4 text-sm text-gray-800">
+                                                {member.qualification}
+                                            </td>
+                                            <td className="px-6 py-4 text-sm font-medium text-right">
+                                                {member.contact}
+                                            </td>
+                                            <td className="px-6 py-4 text-sm font-medium text-right">
+                                                {member.experties}
+                                            </td>
+                                            <td className="px-6 py-4 text-sm font-medium text-right">
+                                                {member.address}
+                                            </td>
+                                        </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>
+                        </table>
                 </div>
             </div>
         </Layout>
